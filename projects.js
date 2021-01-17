69c1ff64cdbee62c1a4900dc97758daaa2e25a2f
Arsh <dhill393>
2021-01-17 18:51:08 -0500

    bug fix

--- projects.js
+++ projects.js
@@ -2,14 +2,16 @@ import {Backend} from "./backend.js";
 const API = new Backend();
 API.setBaseUrl("https://api.github.com/");
 let html = "";
+const table = document.querySelector("#project-table");
 API.get("users/ArshD1230/repos").then(data => {
     data.forEach(repo => {
-        html += `<tr>
-        <td style="width: 25%;"><span class="blue">${repo.full_name}</span></td>
-        <td>${repo.description}</td>
-    </tr>`
+        if (repo.name != "first-contributions" && repo.name != "ArshD1230.github.io") {
+          html += `<tr>
+          <td style="width: 25%;"><span class="blue"><a href="${repo.html_url}" target="_blank">${repo.name}</a></span></td>
+          <td>${repo.description}</td>
+          </tr>`;
+        }
     })
+    table.innerHTML = html;
+    console.log(html);
 });
-console.log(html);
-const table = document.querySelector("#project-table");
-table.innerHTML = html;
